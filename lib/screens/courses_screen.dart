7cd8a7f9ef620016951b8ff0a874fca1e2bb8282
Alok-Verma45 <alokkumar09921@gmail.com>
2026-06-20 17:51:20 +0530

    Add course search functionality

--- lib/screens/courses_screen.dart
+++ lib/screens/courses_screen.dart
@@ -1,19 +1,64 @@
 import 'package:flutter/material.dart';
-import 'course_detail_screen.dart';
+
 import '../data/course_repository.dart';
 import '../models/course.dart';
+import 'course_detail_screen.dart';
 
-class CoursesScreen extends StatelessWidget {
+class CoursesScreen extends StatefulWidget {
   const CoursesScreen({super.key});
 
+  @override
+  State<CoursesScreen> createState() =>
+      _CoursesScreenState();
+}
+
+class _CoursesScreenState
+    extends State<CoursesScreen> {
+
+  final List<Course> allCourses =
+  CourseRepository.getCourses();
+
+  late List<Course> filteredCourses;
+
+  @override
+  void initState() {
+    super.initState();
+
+    filteredCourses = allCourses;
+  }
+
+  void searchCourses(String query) {
+
+    setState(() {
+
+      filteredCourses =
+          allCourses.where((course) {
+
+            return course.title
+                .toLowerCase()
+                .contains(
+              query.toLowerCase(),
+            );
+
+          }).toList();
+    });
+  }
+
   IconData getCourseIcon(String title) {
+
     if (title.contains("Flutter")) {
       return Icons.phone_android;
-    } else if (title.contains("Java")) {
+    }
+
+    if (title.contains("Java")) {
       return Icons.coffee;
-    } else if (title.contains("Spring")) {
+    }
+
+    if (title.contains("Spring")) {
       return Icons.rocket_launch;
-    } else if (title.contains("React")) {
+    }
+
+    if (title.contains("React")) {
       return Icons.code;
     }
 
@@ -22,8 +67,6 @@ class CoursesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final List<Course> courses =
-    CourseRepository.getCourses();
 
     return Scaffold(
       appBar: AppBar(
@@ -38,75 +81,130 @@ class CoursesScreen extends StatelessWidget {
 
           Padding(
             padding: const EdgeInsets.all(16),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: "Search courses...",
-                prefixIcon: const Icon(Icons.search),
 
-                border: OutlineInputBorder(
+            child: TextField(
+
+              onChanged: (value) {
+
+                searchCourses(value);
+
+              },
+
+              decoration: InputDecoration(
+                hintText:
+                "Search courses...",
+
+                prefixIcon:
+                const Icon(Icons.search),
+
+                border:
+                OutlineInputBorder(
                   borderRadius:
-                  BorderRadius.circular(16),
+                  BorderRadius.circular(
+                    16,
+                  ),
                 ),
               ),
             ),
           ),
 
           Expanded(
-            child: ListView.builder(
-              padding: const EdgeInsets.only(
+
+            child: filteredCourses.isEmpty
+
+                ? const Center(
+              child: Text(
+                "No Courses Found",
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            )
+
+                : ListView.builder(
+
+              padding:
+              const EdgeInsets.only(
                 bottom: 16,
               ),
 
-              itemCount: courses.length,
+              itemCount:
+              filteredCourses.length,
 
-              itemBuilder: (context, index) {
-                final course = courses[index];
+              itemBuilder:
+                  (context, index) {
+
+                final course =
+                filteredCourses[index];
 
                 return Card(
-                  margin: const EdgeInsets.symmetric(
+
+                  margin:
+                  const EdgeInsets
+                      .symmetric(
                     horizontal: 16,
                     vertical: 8,
                   ),
 
                   elevation: 4,
 
-                  shape: RoundedRectangleBorder(
+                  shape:
+                  RoundedRectangleBorder(
                     borderRadius:
-                    BorderRadius.circular(20),
+                    BorderRadius
+                        .circular(
+                      20,
+                    ),
                   ),
 
                   child: ListTile(
-                    contentPadding:
-                    const EdgeInsets.all(16),
 
-                    leading: CircleAvatar(
+                    contentPadding:
+                    const EdgeInsets
+                        .all(16),
+
+                    leading:
+                    CircleAvatar(
                       radius: 24,
+
                       child: Icon(
-                        getCourseIcon(course.title),
+                        getCourseIcon(
+                          course.title,
+                        ),
                       ),
                     ),
 
                     title: Text(
                       course.title,
-                      style: const TextStyle(
+
+                      style:
+                      const TextStyle(
                         fontWeight:
-                        FontWeight.bold,
+                        FontWeight
+                            .bold,
+
                         fontSize: 18,
                       ),
                     ),
 
-                    subtitle: Padding(
+                    subtitle:
+                    Padding(
+
                       padding:
-                      const EdgeInsets.only(
+                      const EdgeInsets
+                          .only(
                         top: 8,
                       ),
 
                       child: Column(
+
                         crossAxisAlignment:
-                        CrossAxisAlignment.start,
+                        CrossAxisAlignment
+                            .start,
 
                         mainAxisSize:
-                        MainAxisSize.min,
+                        MainAxisSize
+                            .min,
 
                         children: [
 
@@ -125,19 +223,24 @@ class CoursesScreen extends StatelessWidget {
                       ),
                     ),
 
-                    trailing: const Icon(
-                      Icons.arrow_forward_ios,
+                    trailing:
+                    const Icon(
+                      Icons
+                          .arrow_forward_ios,
                       size: 18,
                     ),
 
                     onTap: () {
+
                       Navigator.push(
                         context,
 
                         MaterialPageRoute(
-                          builder: (context) =>
+                          builder:
+                              (context) =>
                               CourseDetailScreen(
-                                course: course,
+                                course:
+                                course,
                               ),
                         ),
                       );
